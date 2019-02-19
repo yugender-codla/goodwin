9214b0f623acc39ca8be5d1e9c601081a2496560
Yugendar <Yugendar@DESKTOP-9VJO8FQ>
2019-02-19 11:33:12 +0530

    modified to email address

--- goodwin/js/contact_mail.php
+++ goodwin/js/contact_mail.php
@@ -1,5 +1,5 @@
 <?php
-    $toEmail = "yugender.codla@gmail.com";
+    $toEmail = "dilipindia6@gmail.com";
     $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
     if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
         print "<p class='success'>Mail Sent.</p>";
